0fbb75c1a74e56cda93c859fe700eef189901a48
Mayur Kishor Shende <mayurks@uia.no>
2025-07-03 14:50:02 +0000

    celeba experiments

---
+++ experiments/balancetm/celeba/celeba_no_bal.py
@@ -0,0 +1,170 @@
+import os
+import pickle
+from datetime import datetime
+from lzma import LZMAFile
+
+import matplotlib.pyplot as plt
+import numpy as np
+from load import label_names, load_celeba, load_image_batch
+from tqdm import tqdm
+
+import wandb
+from cutm import MultiOutputTM
+from tm_utils import Timer
+from tm_utils.metrics import multilabel_metrics
+
+BATCH_SIZE = 90000
+PROJECT = "IMBALANCE"
+NAME = f"celeba_no_bal_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}"
+DIR  = f"./runs/celeba/{NAME}"
+TAGS = ["celeba", "default"]
+GROUP = "celeba"
+
+
+def encode_data(tm, file, ids, ch):
+    encoded_X = []
+    for i in tqdm(range(0, len(ids), BATCH_SIZE), desc="Encoding", dynamic_ncols=True, leave=False):
+        batch_ids = ids[i : i + BATCH_SIZE]
+        batch_X = load_image_batch(file, batch_ids, ch)
+        encoded_X.append(tm.encode(batch_X))
+    return np.vstack(encoded_X)
+
+def eval(tm: MultiOutputTM, encoded_X, Y):
+    eval_time = 0
+
+    # preds = []
+    # css= []
+    # for i in tqdm(range(0, encoded_X.shape[0], BATCH_SIZE), desc="Evaluating", dynamic_ncols=True, leave=False):
+    #     batch_X_encoded = encoded_X[i : i + BATCH_SIZE]
+    #     eval_timer = Timer()
+    #     with eval_timer:
+    #         batch_preds, batch_css = tm.predict(batch_X_encoded, is_X_encoded=True, block_size=512)
+    #     eval_time += eval_timer.elapsed()
+    #
+    #     preds.append(batch_preds)
+    #     css.append(batch_css)
+    #
+    # preds = np.vstack(preds)
+    # css = np.vstack(css)
+
+    eval_timer = Timer()
+    with eval_timer:
+        preds, css = tm.predict(encoded_X, is_X_encoded=True, block_size=512)
+    eval_time += eval_timer.elapsed()
+
+    metrics = multilabel_metrics(Y, preds, css, label_names)
+    for k, v in metrics.items():
+        if "confusion_matrix" in k:
+            metrics[k] = wandb.Image(v)
+
+    return metrics, eval_time
+
+def train(tm: MultiOutputTM, file, ids_train, Ytrain, ids_val, Yval, ids_test, Ytest, ch, run, epochs=1):
+    print("Encoding training, validation, and test data...")
+    encoded_X_train = encode_data(tm, file, ids_train, ch)
+    encoded_X_val = encode_data(tm, file, ids_val, ch)
+    encoded_X_test = encode_data(tm, file, ids_test, ch)
+
+    print(f"Training for {epochs} epochs")
+
+    for epoch in tqdm(range(epochs), desc="Epochs", dynamic_ncols=True):
+        iota = np.arange(len(ids_train))
+        np.random.shuffle(iota)
+        train_fit_time = 0
+        # for i in tqdm(range(0, len(iota), BATCH_SIZE), desc="Training", dynamic_ncols=True, leave=False):
+        #     indices = iota[i : i + BATCH_SIZE]
+        #     batch_X_encoded = encoded_X_train[indices]
+        #     batch_Y = Ytrain[indices]
+        #
+        #     train_fit_timer = Timer()
+        #     with train_fit_timer:
+        #         tm.fit(batch_X_encoded, batch_Y, is_X_encoded=True)
+        #     train_fit_time += train_fit_timer.elapsed()
+
+        train_fit_timer = Timer()
+        with train_fit_timer:
+            tm.fit(encoded_X_train, Ytrain, is_X_encoded=True)
+        train_fit_time = train_fit_timer.elapsed()
+
+
+        train_met, _ = eval(tm, encoded_X_train, Ytrain)
+        val_met, _ = eval(tm, encoded_X_val, Yval)
+        test_met, test_eval_time = eval(tm, encoded_X_test, Ytest)
+
+
+        run.log(
+            {
+                "epoch": epoch + 1,
+                "fit_time": train_fit_time,
+                "test_time": test_eval_time,
+                **{f"train/{k}": v for k, v in train_met.items()},
+                **{f"val/{k}": v for k, v in val_met.items()},
+                **{f"test/{k}": v for k, v in test_met.items()},
+            }
+        )
+
+        plt.close("all")
+
+
+
+if __name__ == "__main__":
+    file, ids_train, Y_train = load_celeba("./data/CelebA", 0)
+    _, ids_val, Y_val = load_celeba("./data/CelebA", 1)
+    _, ids_test, Y_test = load_celeba("./data/CelebA", 2)
+    ch = 8
+
+    config = {
+        "dataset": "PneumoniaMNIST",
+        "binarization": "thermometer",
+        "binarization_channels": ch,
+        "training_samples": Y_train.shape[0],
+        "validation_samples": Y_val.shape[0],
+        "test_samples": Y_test.shape[0],
+        "n_classes": len(label_names),
+        "label_names": label_names,
+        "number_of_clauses": 10000,
+        "T": 50000,
+        "s": 10,
+        "q": 25,
+        "dim": (64, 64, ch * 3),
+        "patch_dim": (3, 3),
+        "seed": 10,
+        "total_epochs": 50,
+    }
+
+    run = wandb.init(
+        project=PROJECT,
+        name=NAME,
+        tags=TAGS,
+        group=GROUP,
+        save_code=True,
+        config=config,
+    )
+    run.log_code(
+        include_fn=lambda path: ".pixi" not in path
+        and (path.endswith(".py") or path.endswith(".cu") or path.endswith(".toml"))
+    )
+
+    tm = MultiOutputTM(
+        number_of_clauses=config["number_of_clauses"],
+        T=config["T"],
+        s=config["s"],
+        q=config["q"],
+        dim=config["dim"],
+        n_classes=config["n_classes"],
+        patch_dim=config["patch_dim"],
+        seed=config["seed"],
+        block_size=128,
+    )
+
+    train(tm, file, ids_train, Y_train, ids_val, Y_val, ids_test, Y_test, ch, run, epochs=config["total_epochs"])
+
+    os.makedirs(DIR, exist_ok=True)
+    with open(f"{DIR}/config.tsv", "w") as f:
+        for key, value in config.items():
+            f.write(f"{key}\t{value}\n")
+
+    with LZMAFile(f"{DIR}/model.tm", "wb") as f:
+        pickle.dump(tm, f)
+
+    wandb.finish()
--- src/tm_utils/metrics.py
+++ src/tm_utils/metrics.py
@@ -1,13 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from sklearn.metrics import (
-    hamming_loss,
     accuracy_score,
     confusion_matrix,
-    roc_auc_score,
+    hamming_loss,
+    multilabel_confusion_matrix,
     precision_recall_fscore_support,
+    roc_auc_score,
 )
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
 def plot_heatmap(arr, **hmapargs):
@@ -32,6 +33,9 @@ def plot_heatmap(arr, **hmapargs):
 
 
 def multiclass_metrics(true, pred, prob, class_names):
+    assert true.ndim == 1, "True labels should be a 1D array."
+    assert pred.ndim == 1, "Predicted labels should be a 1D array."
+    assert prob.ndim == 2, "Probabilities should be a 2D array."
     N = true.shape[0]
 
     acc = accuracy_score(true, pred)
@@ -39,18 +43,18 @@ def multiclass_metrics(true, pred, prob, class_names):
     cm = confusion_matrix(true, pred)
     cm_plot = plot_heatmap(cm, xticklabels=class_names, yticklabels=class_names)
 
-    macro_pre, macro_rec, macro_f1, _ = precision_recall_fscore_support(true, pred, average="macro", zero_division=0)  # pyright:ignore[reportArgumentType]
+    macro_pre, macro_rec, macro_f1, _ = precision_recall_fscore_support(true, pred, average="macro", zero_division=0)
     per_class_pre, per_class_rec, per_class_f1, _ = precision_recall_fscore_support(
         true,
         pred,
         average=None,
-        zero_division=0,  # pyright:ignore[reportArgumentType]
+        zero_division=0,
     )
 
     true_bin = np.zeros((N, len(class_names)))
     true_bin[np.arange(N), true] = 1
     macro_roc = roc_auc_score(true_bin, prob, average="macro", multi_class="ovr")
-    per_class_roc = roc_auc_score(true_bin, prob, average=None, multi_class="ovr")  # pyright:ignore[reportArgumentType]
+    per_class_roc = roc_auc_score(true_bin, prob, average=None, multi_class="ovr")
 
     metrics = {
         "accuracy": acc,
@@ -63,6 +67,50 @@ def multiclass_metrics(true, pred, prob, class_names):
         **{f"f1/class.{name}": f1 for name, f1 in zip(class_names, per_class_f1)},  # pyright:ignore[reportArgumentType]
         **{f"roc_auc/class.{name}": roc for name, roc in zip(class_names, per_class_roc)},  # pyright:ignore[reportArgumentType]
         "confusion_matrix": cm_plot,
+        "hamming_loss": hml,
+    }
+
+    return metrics
+
+
+def multilabel_metrics(true, pred, prob, class_names):
+    assert true.ndim == 2, "True labels should be a 2D array."
+    assert pred.ndim == 2, "Predicted labels should be a 2D array."
+    assert prob.ndim == 2, "Probabilities should be a 2D array."
+
+    N = true.shape[0]
+
+    acc = 0
+    for c in range(len(class_names)):
+        acc += accuracy_score(true[:, c], pred[:, c])
+        acc /= len(class_names)
+
+    cm = multilabel_confusion_matrix(true, pred)
+    cm_plots = [plot_heatmap(cm[i], xticklabels=["0", "1"], yticklabels=["0", "1"]) for i in range(len(class_names))]
+
+    hml = hamming_loss(true, pred)
+    macro_pre, macro_rec, macro_f1, _ = precision_recall_fscore_support(true, pred, average="macro", zero_division=0)
+    per_class_pre, per_class_rec, per_class_f1, _ = precision_recall_fscore_support(
+        true,
+        pred,
+        average=None,
+        zero_division=0,
+    )
+
+    macro_roc = roc_auc_score(true, prob, average="macro", multi_class="ovr")
+    per_class_roc = roc_auc_score(true, prob, average=None, multi_class="ovr")
+
+    metrics = {
+        "accuracy": acc,
+        "precision/macro": macro_pre,
+        "recall/macro": macro_rec,
+        "f1/macro": macro_f1,
+        "roc_auc/macro": macro_roc,
+        **{f"precision/class.{name}": pre for name, pre in zip(class_names, per_class_pre)},  # pyright:ignore[reportArgumentType]
+        **{f"recall/class.{name}": rec for name, rec in zip(class_names, per_class_rec)},  # pyright:ignore[reportArgumentType]
+        **{f"f1/class.{name}": f1 for name, f1 in zip(class_names, per_class_f1)},  # pyright:ignore[reportArgumentType]
+        **{f"roc_auc/class.{name}": roc for name, roc in zip(class_names, per_class_roc)},  # pyright:ignore[reportArgumentType]
+        **{f"confusion_matrix/class.{name}": cm_plot for name, cm_plot in zip(class_names, cm_plots)},  # pyright:ignore[reportArgumentType]
         "hamming_loss": hml,
     }
 
