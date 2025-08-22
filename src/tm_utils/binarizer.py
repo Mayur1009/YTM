3cf5ecfd124a6ae1c70bcf210b986ca9d51a62d9
Mayur Kishor Shende <mayurks@uia.no>
2025-08-22 11:42:31 +0000

    binarizer update for better type hints

--- src/tm_utils/binarizer.py
+++ src/tm_utils/binarizer.py
@@ -8,11 +8,13 @@ class ThermometerBinarizer:
     def binarize_gray(self, X):
         assert X.ndim == 3, "Input should be a 3D array (N, H, W)"
         thresholds = (np.arange(1, self.ch + 1) * 255 / (self.ch + 1)).reshape(1, 1, 1, self.ch)
-        out = (X[:, :, :, None] >= thresholds).astype(np.uint32)
+        out = np.asarray(X[:, :, :, None] >= thresholds, dtype=np.uint32).reshape(
+            (X.shape[0], X.shape[1], X.shape[2], self.ch)
+        )
         return out
 
     def binarize_rgb(self, X):
         assert X.ndim == 4, "Input should be a 4D array (N, H, W, C)"
         thresholds = (np.arange(1, self.ch + 1) * 255 / (self.ch + 1)).reshape(1, 1, 1, 1, self.ch)
-        out = (X[:, :, :, :, None] >= thresholds).astype(np.uint32)
-        return out.reshape((*X.shape[:-1], X.shape[-1] * self.ch))
+        out = (X[:, :, :, :, None] >= thresholds).reshape((X.shape[0], X.shape[1], X.shape[2], X.shape[3] * self.ch))
+        return np.asarray(out, dtype=np.uint32)
