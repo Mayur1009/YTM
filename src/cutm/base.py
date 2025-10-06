94bb3ec46a421bd709c612d7935441579ff158f0
Mayur Kishor Shende <mayurks@uia.no>
2025-10-06 12:19:13 +0000

    Better encode

--- src/cutm/base.py
+++ src/cutm/base.py
@@ -1,6 +1,5 @@
 import pathlib
 from typing import TypedDict, Unpack
-import warnings
 
 import numpy as np
 import pycuda.autoinit  # noqa: F401
@@ -274,8 +273,7 @@ class BaseTM:
     ) -> np.ndarray[tuple[int, int, int], np.dtype[np.uint32]]:
         assert X.dtype == np.uint32, "X must be of type np.uint32."
         if X.ndim > 2:
-            warnings.warn(f"X has shape = {X.shape}. Collapsing to 2D array. New shape: {(X.shape[0], int(np.prod(X.shape[1:])))}")
-            X = X.reshape((X.shape[0], -1))
+            X = X.reshape((X.shape[0], self.dim[0] * self.dim[1] * self.dim[2]))
 
         N = X.shape[0]
 
